38facf3cf96f5950afadb41fa2e036cff96dc4ca
Danny Lake <dannytlake@gmail.com>
2023-05-23 14:41:25 -0700

    init

---
+++ composable-ui/next.config.js
@@ -0,0 +1,59 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const NO_INDEX_FOLLOW_PATHS = ['/cart', '/checkout', '/checkout/success']
+
+// Security headers
+const HEADERS = {
+  'X-Frame-Options': 'DENY',
+  'X-Content-Type-Options': 'nosniff',
+  'Referrer-Policy': 'same-origin',
+  'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+  'Content-Security-Policy':
+    "img-src 'self'; base-uri 'self'; child-src 'self'",
+  'Permissions-Policy':
+    'camera=(self), microphone=(self), geolocation=(self), interest-cohort=(self)',
+}
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  transpilePackages: [
+    '@composable/cms-generic',
+    '@composable/commerce-generic',
+    '@composable/stripe',
+    '@composable/types',
+    '@composable/ui',
+  ],
+  images: {
+    domains: ['loremflickr.com'],
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  experimental: {
+    esmExternals: 'loose',
+  },
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: Object.entries(HEADERS).map(([key, value]) => ({
+          key,
+          value,
+        })),
+      },
+      ...NO_INDEX_FOLLOW_PATHS.map((source) => ({
+        source,
+        headers: [
+          {
+            key: 'x-robots-tag',
+            value: 'noindex, follow',
+          },
+        ],
+      })),
+    ]
+  },
+})
---
+++ composable-ui/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { Box } from '@chakra-ui/react'
+
+const HomePage = () => <Box>Homepage</Box>
+
+export default HomePage
